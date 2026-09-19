0c4f924c296fd1f9de6ce86205e18d04d098713a
parimallpradhan <153976296+parimallpradhan@users.noreply.github.com>
2026-09-19 16:29:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,25 @@ resource "aws_instance" "web" {
   ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = "c7i-flex.large"
 
- 
+  user_data = <<-EOF
+              #!/bin/bash
+
+              sudo apt update
+sudo apt install fontconfig openjdk-21-jre
+java -version
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins
+
+              EOF
 
   tags = {
-    Name = "Terraform-Web-Server"
+    Name = "Terraform-Auto-Instance"
   }
 }
 
